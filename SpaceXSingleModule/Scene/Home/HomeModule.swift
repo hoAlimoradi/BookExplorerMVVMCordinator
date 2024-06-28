5c4631f3881a5cdb6efffb2b832dd27db378c40a
hosein alimoradi <ho.alimoradi@gmail.com>
2024-06-29 02:12:48 +0330

    added Home Structure

---
+++ SpaceXSingleModule/Scene/Home/HomeModule.swift
@@ -0,0 +1,8 @@
+//
+//  HomeModule.swift
+//  SpaceXSingleModule
+//
+//  Created by ho on 4/9/1403 AP.
+//
+
+import Foundation
---
+++ SpaceXSingleModule/Scene/Home/ViewModel/HomeViewModelProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModelProtocol.swift
+//  SpaceXSingleModule
+//
+//  Created by ho on 4/9/1403 AP.
+//
+
+import Foundation
