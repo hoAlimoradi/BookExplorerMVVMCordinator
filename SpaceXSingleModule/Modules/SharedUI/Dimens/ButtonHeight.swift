95748feb463c7d92681244a48261425a4c835c9f
hosein alimoradi <ho.alimoradi@gmail.com>
2024-06-28 18:42:31 +0330

    added Dimens

---
+++ SpaceXSingleModule/Modules/SharedUI/Dimens/ButtonHeight.swift
@@ -0,0 +1,12 @@
+//
+//  ButtonHeight.swift
+//  SpaceXSingleModule
+//
+//  Created by ho on 4/8/1403 AP.
+//
+import Foundation
+struct ButtonHeight {
+    static var large56: CGFloat = 56
+    static var medium44: CGFloat = 44
+    static var small32: CGFloat = 32
+}
